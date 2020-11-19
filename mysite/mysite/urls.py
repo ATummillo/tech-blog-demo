239e57b5dba0fa1f30ba6cb09617b6d60db82363
Anthony Tummillo <atummillo18@gmail.com>
2020-11-19 10:48:36 -0800

    Added urls and templates for user login

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,11 @@ from django.contrib.auth import views
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path(
+        'accounts/logout/',
+        views.LogoutView.as_view(),
+        name='logout',
+        kwargs={'next_page': '/'},
+    ),
 ]
